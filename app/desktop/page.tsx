1e4a50f83c5346a22cbe985a702a9e81ead8c2d1
alekspiter2-cmyk <alekspiter2@gmail.com>
2026-07-16 20:19:17 +0300

    Add responsive Petshop catalog pages

--- app/desktop/page.tsx
+++ app/desktop/page.tsx
@@ -9,7 +9,7 @@ export default function DesktopPreview() {
       </nav>
       <iframe
         className="prototype-frame desktop-frame"
-        src="/prototype/index.html?v=3"
+        src="/prototype/index.html?v=4"
         title="Десктопный прототип Petshop.ru"
       />
     </main>
